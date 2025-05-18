aad8f5c9e76146b64bb5ce18ff2a2197e568e8d6
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2025-05-18 21:35:57 +0530

    file added

---
+++ src/main/java/lk/ac/vau/fas/ict/model/Project.java
@@ -0,0 +1,17 @@
+package lk.ac.vau.fas.ict.model;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+
+@Entity
+public class Project {
+	@Id
+	private int id;
+	private String name;
+	private long totalcost;
+	@ManyToMany
+	private List<Employee>employees;
+}
